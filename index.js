ac780c31999d8d0427fc2e18bfaa6fc8741977a0
VictoriaCarvalho <vix219@icloud.com>
2025-02-02 09:02:03 -0600

    add lab

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Code your solution in this file!
+
+const arrayOfDrivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+
+function returnFirstTwoDrivers() {
+  return arrayOfDrivers.slice(0, 2);
+}
+
+
+function returnLastTwoDrivers() {
+  return arrayOfDrivers.slice(2);
+}
+console.log(returnFirstTwoDrivers()); 
+console.log(returnLastTwoDrivers());  
+
+const selectingDrivers = [
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers,
+]
+selectingDrivers[0, 1];
+
+
+
+
+
+function createFareMultiplier() {
+    return function(number) {
+      return number * 5;
+    };
+  }
+
+
+  function fareDoubler() {
+    let number = 10
+      return number * 2;
+    };
+
+ 
+    function fareTripler() {
+        let number = 12
+        return number * 3;
+      };
+
+     function selectDifferentDrivers(arrayOfDrivers, returnFirstTwoDrivers) {
+        return returnFirstTwoDrivers(); 
+}
+
+    
